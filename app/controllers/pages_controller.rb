d7a29f82da3d45ce23bc899f3a779f21f463ddbf
Constant Desfosse <desfosse.constant@gmail.com>
2017-11-28 17:06:22 +0100

    controller pages + root

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def accueil
+  end
+
+  def restaurant
+  end
+
+  def menu
+  end
+
+  def produits
+  end
+
+  def recettes
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  root to: "pages#accueil"
+
+  get 'pages/accueil'
+
+  get 'pages/restaurant'
+
+  get 'pages/menu'
+
+  get 'pages/produits'
+
+  get 'pages/recettes'
+
+  get 'pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
