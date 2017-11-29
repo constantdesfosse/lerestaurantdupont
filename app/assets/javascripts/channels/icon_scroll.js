58410b300b2664a47be6ce38d588fad8cca97e28
Constant Desfosse <desfosse.constant@gmail.com>
2017-11-29 21:33:51 +0100

    adding js to home

--- app/assets/javascripts/channels/icon_scroll.js
+++ app/assets/javascripts/channels/icon_scroll.js
@@ -3,5 +3,5 @@ function FaireClignoterImage (){
    $("#icon_scroll").fadeOut(2200).delay(10).fadeIn(2200);
 }
 $(document).ready(function(){
-    setInterval('FaireClignoterImage()',3000);
+    setInterval('FaireClignoterImage()',2000);
 });
