353f4f36cf40c1447b81e5a5b3d19cc6a3e8e496
Constant Desfosse <desfosse.constant@gmail.com>
2018-03-13 20:45:49 +0100

    mdfy prix, tel

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < MailForm::Base
   attribute :nom,      :validate => true
+  attribute :telephone,      :validate => /\A\d{10}\z/
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
   attribute :nickname,  :captcha  => true
@@ -8,7 +9,8 @@ class Contact < MailForm::Base
     {
       :subject => "Message pour le Restaurant du Pont",
       :to => "sask2mc@gmail.com",
-      :from => %("#{nom}" <#{email}>)
+      :from => %("#{nom}" <#{email}>),
+      :telephone => %("#{telephone}")
     }
   end
 end
